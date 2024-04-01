bf89156bdd20285a79ef5fc6ccebbadfd01781e8
alex <ra693@gmail.com>
2024-04-01 11:49:13 +0300

    Таблицы связи

---
+++ database/migrations/2024_04_01_084359_user_group.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_group', function (Blueprint $table) {
+            $table->comment('Участие пользователя в группе');
+            $table->id();
+            $table->timestamps();
+            $table->bigInteger('user_id')->unsigned()->comment('Пользователь');
+            $table->bigInteger('group_id')->unsigned()->comment('Группа');
+            $table->text('role')->comment('Роль в группе');
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreign('group_id')->references('id')->on('groups')
+                ->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_group');
+    }
+};
---
+++ database/migrations/2024_04_01_084502_create_attendances_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->bigInteger('school_day_id')->unsigned()->comment('Учебный день группы');
+
+            $table->foreign('school_day_id')->references('id')->on('school_days')
+                ->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
