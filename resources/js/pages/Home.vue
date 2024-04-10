0d741ae70ae2bc16fea21871e738abc91e8ac4be
Игорь <lotar76@gmail.com>
2024-04-10 12:39:47 +0300

    init vue

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -3,14 +3,24 @@
 </script>
 
 <template>
-  <div class="tw-flex tw-flex-row m-2">
-    <v-text-field base-color="red" variant="outlined">asdf</v-text-field>
-    <h2>tw-www</h2>
-    <h1>Home Component Welcome</h1>
+  <div class="main">
+    <h1>Главный дашборд</h1>
+    <h6>Будут ли отличия взависимости от роли?</h6>
   </div>
 
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.main{
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  h6{
+    color: gray;
+    font-size: 20px;
+  }
+}
 </style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Школа</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Школа</title>
 
-    </head>
-    <body>
-    <div class="tw-w-full tw-h-full tw-bg-red-100 ">
-sss
-    </div>
+</head>
+<body style="margin: 0; padding: 0">
 
-    </body>
+
+<div style="height: 100vh;background-image: url('https://cms-imgp.jw-cdn.org/img/p/1102016050/univ/art/1102016050_univ_lsr_md.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;"></div>
+</body>
+
 </html>
