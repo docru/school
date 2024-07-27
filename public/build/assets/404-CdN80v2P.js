834a8a0e813f0e86923435589e79e3dac400ca62
alex <ra693@gmail.com>
2024-07-27 10:49:45 +0300

    сборка

---
+++ public/build/assets/404-CdN80v2P.js
@@ -0,0 +1,1 @@
+import{_ as t,v as e}from"./app-DAbPShih.js";const o={name:"404"};function n(r,c,s,a,_,p){return e.openBlock(),e.createElementBlock("h1",null,"404")}const l=t(o,[["render",n]]);export{l as default};
