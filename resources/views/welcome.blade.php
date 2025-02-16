e2c4b155651462da1d6ee606505c8430c5c3293a
alex <ra693@gmail.com>
2025-02-16 20:17:58 +0300

    заставка у неавторизованного пользователя

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Школа</title>
+    <title>CXM</title>
 
 </head>
 <body style="margin: 0; padding: 0">
 
 
-<div style="height: 100vh;background-image: url('https://cms-imgp.jw-cdn.org/img/p/1102016050/univ/art/1102016050_univ_lsr_md.jpg');
+<div style="height: 100vh;background-image: url('/img/david.jpg');
             background-repeat: no-repeat;
             background-size: cover;background-position: center center;"></div>
 </body>
